b8aec6023e842365d4a98c5b9a1ba58c9d2e139f
Batuhan Yildirim <44163664+by256@users.noreply.github.com>
2020-09-02 08:49:03 +0100

    updated get_mpdata and mp-ids

--- cgcnn/data/get_mpdata.py
+++ cgcnn/data/get_mpdata.py
@@ -1,125 +1,42 @@
 import csv
 import json
 import requests
-
-import pprint
-
- 
-#args
-max_sites = 50
+import argparse
+import pandas as pd
+from pprint import pprint
+from pymatgen import MPRester
 
 
-with open('./training-data-mp-ids.csv', 'r') as f:
-    reader = csv.reader(f)
-    mp_ids = [x[0] for x in list(reader)]
+parser = argparse.ArgumentParser(description="Query materialsproject API and download CIFs for training the CGCNN")
+parser.add_argument('--key', metavar='key', type=str, help='Matrials Project API key')
+parser.add_argument('--mp-ids', metavar='ids', type=str, help='path to file containing mp-ids to query.', default='./training-data-mp-ids.csv')
+parser.add_argument('--saveto', metavar='saveto', type=str, help='target path to save data', default='./structure-property-data.csv')
+namespace = parser.parse_args()
 
-api_key = 'INSERT KEY HERE'
-base_url = 'https://www.materialsproject.org/rest/v1/materials/'
-csv_data = []
-col_names = ['mp_id', 'full_formula', 'unit_cell_formula', 'formation_energy_per_atom', 
-            'energy_per_atom', 'band_gap', 'bulk_modulus', 'shear_modulus', 'poisson', 
+mp_ids = pd.read_csv(namespace.mp_ids, header=None).drop_duplicates()
+mp_ids = list(mp_ids[0])
+
+api_key = namespace.key
+mpr = MPRester(api_key)
+
+criteria = {'material_id': {'$in': mp_ids}}
+
+properties = ['material_id', 'full_formula', 'pretty_formula', 'nsites', 'unit_cell_formula', 
+            'formation_energy_per_atom', 
+            'energy_per_atom', 'e_above_hull', 'band_gap', 'elasticity.K_Voigt_Reuss_Hill', 
+            'elasticity.G_Voigt_Reuss_Hill', 'elasticity.poisson_ratio', 'diel.poly_electronic', 
+            'diel.e_electronic', 'diel.n', 'piezo.eij_max', 'piezo.piezoelectric_tensor', 
+            'total_magnetization', 'crystal_system', 'cif']
+
+entries = mpr.query(criteria=criteria, properties=properties)
+
+col_names = ['mp_id', 'full_formula', 'pretty_formula',  'nsites', 'unit_cell_formula', 
+             'formation_energy_per_atom', 
+            'energy_per_atom', 'e_above_hull', 'band_gap', 'bulk_modulus', 'shear_modulus', 'poisson', 
             'dielectric_constant', 'dielectric_tensor', 'refractive_index', 
             'eij_max', 'piezoelectric_tensor', 'total_magnetization', 'crystal_system', 'cif']
-csv_data.append(col_names)
 
-all_exceptions = []
+col_name_mapper = dict(zip(properties, col_names))
 
-for i, mp_id in enumerate(mp_ids):
-    try:
-        url = base_url + '{}/vasp'.format(mp_id)
-        response = requests.get(url, headers={'X-API-KEY': api_key}, timeout=15)
-        data = json.loads(response.content)['response'][0]
-        
-        if data['nsites'] > max_sites:
-            continue
-
-        material_id = data['material_id']
-        full_formula = data['full_formula']
-        unit_cell_formula = data['unit_cell_formula']
-        formation_energy_per_atom = data['formation_energy_per_atom']
-        energy_per_atom = data['energy_per_atom']
-        band_gap = data['band_gap']
-
-        # mechanical properties
-        if data['elasticity']:
-            if 'K_Voigt_Reuss_Hill' in data['elasticity']:
-                bulk_modulus = data['elasticity']['K_Voigt_Reuss_Hill']
-            else:
-                bulk_modulus = None
-            if 'G_Voigt_Reuss_Hill' in data['elasticity']:
-                shear_modulus = data['elasticity']['G_Voigt_Reuss_Hill']
-            else:
-                shear_modulus = None
-            if 'poisson_ratio' in data['elasticity']:
-                poisson = data['elasticity']['poisson_ratio']
-            else:
-                poisson = None
-        else:
-            bulk_modulus, shear_modulus, poisson = None, None, None
-
-        # dielectric properties
-        if data['diel']:
-            if data['diel']['poly_electronic']:
-                dielectric_constant = data['diel']['poly_electronic']
-            else:
-                dielectric_constant = None
-            if data['diel']['e_electronic']:
-                dielectric_tensor = data['diel']['e_electronic']
-            else:
-                dielectric_tensor
-            if data['diel']['n']:
-                refractive_index = data['diel']['n']
-            else:
-                refractive_index = None
-        else:
-            dielectric_constant, dielectric_tensor, refractive_index = None, None, None
-
-        # piezoelectric properties
-        if data['piezo']:
-            if data['piezo']['eij_max']:
-                eij_max = data['piezo']['eij_max']
-            else:
-                eij_max = None
-            if data['piezo']['piezoelectric_tensor']:
-                piezoelectric_tensor = data['piezo']['piezoelectric_tensor']
-            else:
-                piezoelectric_tensor = None
-        else:
-            eij_max, piezoelectric_tensor = None, None
-
-        if data['total_magnetization']:
-            total_magnetization = data['total_magnetization']
-        else:
-            total_magnetization = None
-
-        crystal_system = data['spacegroup']['crystal_system']
-
-        cif = data['cif']
-
-        csv_entry = [material_id, full_formula, unit_cell_formula, formation_energy_per_atom, 
-                    energy_per_atom, band_gap, bulk_modulus, shear_modulus, poisson, 
-                    dielectric_constant, dielectric_tensor, refractive_index, 
-                    eij_max, piezoelectric_tensor, total_magnetization, crystal_system, cif]
-        
-        csv_data.append(csv_entry)
-
-    except KeyError as e:
-        pass
-
-    print('{}/{}'.format(i+1, len(mp_ids)), end='\r', flush=True)
-
-    
-    # if i > 5:
-    #     break
-
-# print('EXCEPTIONS', all_exceptions)
-
-with open('structure-property-data.csv', 'w') as f:
-    writer = csv.writer(f)
-    writer.writerows(csv_data)
-
-# import pandas as pd
-# df = pd.read_csv('structure-property-data.csv')
-# # print(df.head())
-# print('\n\n\n', 'SHAPE', df.shape)
-
+df = pd.DataFrame(entries).rename(mapper=col_name_mapper, axis=1)
+df.to_csv(namespace.saveto, index=False)
